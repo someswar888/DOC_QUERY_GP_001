679fea1165a89ee159502965186937ee7d02c87e
Someswar Rao Patike <patikesomeswar19@gmail.com>
2025-03-08 19:30:38 +0530

    first commit

---
+++ myapp.py
@@ -0,0 +1,20 @@
+
+def get_text_chunks(text):
+    text_splitter = RecursiveCharacterTextSplitter(chunk_size=1000, chunk_overlap=20)
+    chunks = text_splitter.split_text(text)
+    return chunks
+
+def get_conversational_chain(vector_store):
+    llm = GooglePalm()
+    memory = ConversationBufferMemory(memory_key="chat_history", return_messages=True)
+    conversation_chain = ConversationalRetrievalChain.from_llm(llm=llm, retriever=vector_store.as_retriever(), memory=memory)
+    return conversation_chain
+
+def user_input(user_question):
+    response = st.session_state.conversation({'question': user_question})
+    st.session_state.chatHistory = response['chat_history']
+    for i, message in enumerate(st.session_state.chatHistory):
+        if i % 2 == 0:
+            st.write("Human:", message.content)
+        else:
+            st.write("Bot: ", message.content)
